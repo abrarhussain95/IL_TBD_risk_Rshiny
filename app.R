590d2d7480d1d91c4ffe1a38e986f3c650bf389e
Abrar Hussain <abrar.arid@gmail.com>
2026-04-16 14:59:42 -0500

    initial version of Rshiny App

---
+++ app.R
@@ -0,0 +1,49 @@
+library(shiny)
+library(htmltools)
+
+ui <- fluidPage(
+  tags$head(
+    tags$style(HTML("
+      .container-fluid {
+        width: 100%;
+        max-width: 1600px;
+      }
+      iframe {
+        background-color: white;
+      }
+    "))
+  ),
+  
+  titlePanel("Interactive Monthly Tick Risk Maps in Illinois"),
+  
+  tabsetPanel(
+    tabPanel(
+      tags$i("Amblyomma americanum"),
+      br(),
+      tags$iframe(
+        src = "map_aa_monthly.html",
+        style = "width:100%; height:850px; border:none;"
+      )
+    ),
+    tabPanel(
+      tags$i("Dermacentor variabilis"),
+      br(),
+      tags$iframe(
+        src = "map_dv_monthly.html",
+        style = "width:100%; height:850px; border:none;"
+      )
+    ),
+    tabPanel(
+      tags$i("Ixodes scapularis"),
+      br(),
+      tags$iframe(
+        src = "map_is_monthly.html",
+        style = "width:100%; height:850px; border:none;"
+      )
+    )
+  )
+)
+
+server <- function(input, output, session) {}
+
+shinyApp(ui, server)
