df31cc8c4e5a3cd3c90fcddc9f65160e9e7e4ff3
Abrar Hussain <abrar.arid@gmail.com>
2026-05-07 19:27:28 -0500

    remove IDW heatmap files and revert to county maps only

--- app.R
+++ app.R
@@ -17,71 +17,30 @@ ui <- fluidPage(
   titlePanel("Monthly tick-borne diseases' risk in Illinois"),
   
   tabsetPanel(
-    
-    # ── COUNTY BASED MAPS ──
     tabPanel(
-      "County Based Maps",
+      tags$span(tags$i("Amblyomma americanum"), " (Lone Star Tick)"),
       br(),
-      tabsetPanel(
-        tabPanel(
-          tags$span(tags$i("Amblyomma americanum"), " (Lone Star Tick)"),
-          br(),
-          tags$iframe(
-            src = "map_aa_monthly.html",
-            style = "width:100%; height:850px; border:none;"
-          )
-        ),
-        tabPanel(
-          tags$span(tags$i("Dermacentor variabilis"), " (American Dog Tick)"),
-          br(),
-          tags$iframe(
-            src = "map_dv_monthly.html",
-            style = "width:100%; height:850px; border:none;"
-          )
-        ),
-        tabPanel(
-          tags$span(tags$i("Ixodes scapularis"), " (Black-legged Tick)"),
-          br(),
-          tags$iframe(
-            src = "map_is_monthly.html",
-            style = "width:100%; height:850px; border:none;"
-          )
-        )
+      tags$iframe(
+        src = "map_aa_monthly.html",
+        style = "width:100%; height:850px; border:none;"
       )
     ),
-    
-    # ── HEAT MAPS ──
     tabPanel(
-      "Heat Maps",
+      tags$span(tags$i("Dermacentor variabilis"), " (American Dog Tick)"),
       br(),
-      tabsetPanel(
-        tabPanel(
-          tags$span(tags$i("Amblyomma americanum"), " (Lone Star Tick)"),
-          br(),
-          tags$iframe(
-            src = "map_aa_idw.html",
-            style = "width:100%; height:850px; border:none;"
-          )
-        ),
-        tabPanel(
-          tags$span(tags$i("Dermacentor variabilis"), " (American Dog Tick)"),
-          br(),
-          tags$iframe(
-            src = "map_dv_idw.html",
-            style = "width:100%; height:850px; border:none;"
-          )
-        ),
-        tabPanel(
-          tags$span(tags$i("Ixodes scapularis"), " (Black-legged Tick)"),
-          br(),
-          tags$iframe(
-            src = "map_is_idw.html",
-            style = "width:100%; height:850px; border:none;"
-          )
-        )
+      tags$iframe(
+        src = "map_dv_monthly.html",
+        style = "width:100%; height:850px; border:none;"
+      )
+    ),
+    tabPanel(
+      tags$span(tags$i("Ixodes scapularis"), " (Black-legged Tick)"),
+      br(),
+      tags$iframe(
+        src = "map_is_monthly.html",
+        style = "width:100%; height:850px; border:none;"
       )
     )
-    
   )
 )
 
